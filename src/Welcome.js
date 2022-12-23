84b09f130b4a3d66fa3dc5f780d00e8d128b4dfd
Salvatore Vella <salvatorevella1234@gmail.com>
2022-12-23 19:10:00 +0100

    Exercise complete

---
+++ src/Welcome.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Age from "./Age";
+
+export default class Welcome extends React.Component {
+    static defaultProps = {
+        name: "Gianmai",
+    }
+    render() {
+        return (
+            <div>
+                <h1>Hello, {this.props.name}!</h1>
+                <Age age={this.props.age}/>
+            </div>
+        )
+    }
+}
